5413b9449825ce782916fc8ce9c69057824c39a8
Yang <3317358338@qq.com>
2026-04-02 17:12:11 +0800

    目标点改为动态，实现动态追踪

--- lqr_cart_control/lqr_node.py
+++ lqr_cart_control/lqr_node.py
@@ -8,6 +8,8 @@ from .lqr_math import compute_lqr_gain
 class LQRControllerNode(Node):
     def __init__(self):
         super().__init__("lqr_node")
+        #获取当前时间
+        self.state_time=self.get_clock().now()
         #动力学参数（A,B） 要和物理法则一致
         self.A=np.array([[0.0,1.0],[0.0,-0.1]])
         self.B=np.array([[0.0],[1.0]])
@@ -30,6 +32,15 @@ class LQRControllerNode(Node):
         self.pub_u = self.create_publisher(Float32, '/lqr/control_input', 10)
 
     def state_callback(self,msg):
+        #计算时间差
+        now=self.get_clock().now()
+        t=(now-self.state_time).nanoseconds /1e9
+
+        #这里让目标点呈时间正弦变化，追踪动态目标点
+        target_p=5.0*np.sin(0.5*t)
+        target_v = 2.5 * np.cos(0.5 * t)
+        self.target = np.array([[target_p], [target_v]])
+
         #处理v p数据
         current = np.array([[msg.data[0]], [msg.data[1]]])
         #计算误差 (Error = Current - Target)
