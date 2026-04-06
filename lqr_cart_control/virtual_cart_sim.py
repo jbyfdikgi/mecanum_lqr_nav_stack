755bfd8c310105ba16c7498be751db0e953de530
Yang <3317358338@qq.com>
2026-04-06 11:36:58 +0800

    修改小车为2D移动

--- lqr_cart_control/virtual_cart_sim.py
+++ lqr_cart_control/virtual_cart_sim.py
@@ -1,44 +1,60 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32MultiArray , Float32
-import random
-
+from std_msgs.msg import Float32MultiArray
+from geometry_msgs.msg import Twist
+import math
 
 class VirtualCartSim(Node):
     def __init__(self):
         super().__init__("virtual_cart_sim")
-        #物理状态初始化 position velocity
-        self.p=0.0
-        self.v=0.0
-        #控制输入初始化 推力
-        self.u=0.0
-        #创建订阅者 代替cmd_vel
-        self.sub_control=self.create_subscription(Float32,"/lqr/control_input",self.control_callback,10)
-        #发布者 代替odom
-        self.pub_state=self.create_publisher(Float32MultiArray,'/cart/state', 10)
-        # 创建Timer
+        #初始物理状态
+        self.x=0.0
+        self.y=0.0
+        self.theta=0.0 #弧度制
+        #输入控制初始化
+        self.v_x=0.0
+        self.v_y=0.0
+        self.omega=0.0
+
+        #订阅cmd_vel
+        self.sub_cmd=self.create_subscription(Twist,"cmd_vel",self.cmd_callback,10)
+        
+        #发布小车状态
+        self.pub_odom=self.create_publisher(Float32MultiArray,"/cart/state_2d",10)
+
+        #定时计算小车物理状态
         self.dt=0.01
-        self.timer=self.create_timer(self.dt,self.math_cal)
+        self.timer=self.create_timer(self.dt,self.physics_step)
 
-        
-    def control_callback(self,msg):
-        #根据反馈修改力控制
-        self.u=msg.data
+        self.get_logger().info("虚拟小车仿真启动")
 
-    def math_cal(self):
-        #牛顿第二定律算加速度 m=1 风阻为0.1倍的v
-        a = self.u - 0.1 * self.v
-        #欧拉法数值积分
-        self.v += a * self.dt
-        self.p += self.v * self.dt
-        #加入高斯噪音模拟现实编码器误差（根据中心极限定理，多个误差累加最终会显示出正态分布特性）
-        noisy_p=self.p+random.gauss(0.0,0.05) #填入均值 方差
-        noisy_v=self.v+random.gauss(0.0,0.05) #填入均值 方差
-        #通过'/cart/state'发布小车虚拟状态
-        state_msg=Float32MultiArray()
-        state_msg.data=[noisy_p,noisy_v]
-        self.pub_state.publish(state_msg)
+    def cmd_callback(self,msg):
+        #从Twist信息中获取三个控制自由度
+        self.v_x = msg.linear.x
+        self.v_y = msg.linear.y
+        self.omega = msg.angular.z
         
+        
+
+    def physics_step(self):       
+        #计算在当前偏航角theta下，局部速度在全局X,Y下的投影
+        dx=self.v_x*math.cos(self.theta)-self.v_y*math.sin(self.theta)
+        dy=self.v_x*math.sin(self.theta)+self.v_y*math.cos(self.theta)
+        dtheta = self.omega
+
+        #数值积分 
+        self.x += dx * self.dt
+        self.y += dy * self.dt
+        self.theta += dtheta * self.dt
+
+        # 将角度规范化到-pi到pi 
+        self.theta = math.atan2(math.sin(self.theta), math.cos(self.theta))
+
+        #发布状态
+        state_msg = Float32MultiArray()
+        state_msg.data = [self.x, self.y, self.theta]
+        self.pub_odom.publish(state_msg)
+
 
 def main(args=None):
     rclpy.init(args=args)
