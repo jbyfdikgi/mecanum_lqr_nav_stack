941ee7c0010e9e9d36bc3897b681fad80545521a
Yang <3317358338@qq.com>
2026-04-14 11:03:35 +0800

    添加LTV-LQR 与前馈补偿

--- lqr_cart_control/lqr_math.py
+++ lqr_cart_control/lqr_math.py
@@ -1,21 +1,34 @@
 import numpy as np
 #Scientific Python (科学计算库)
 import scipy.linalg as la
+import math
 
-def compute_lqr_gain(A,B,Q,R):
+class LqrMathEngine:
+    def __init__(self):
+        #Q R对角矩阵
+        self.Q=np.diag([10.0,10.0,10.0])
+        self.R=np.diag([0.1,0.1,0.1])
 
+    def get_linearized_dynamics(self,theta,v_x,v_y):
+        #计算动态A B矩阵
+        A=np.zeros((3,3))
+        #对theta求偏导的结果
+        A[0, 2] = -v_x * math.sin(theta) - v_y * math.cos(theta)
+        A[1, 2] =  v_x * math.cos(theta) - v_y * math.sin(theta)        
 
-    #Solve Continuous Algebraic Riccati Equation（求解连续代数黎卡提方程）
-    P=la.solve_continuous_are(A,B,Q,R)
-
-    """
-    inverse求解逆矩阵 
-    增益矩阵 K的计算公式是 K = R^{-1} B^T P。
-    在 Python (NumPy) 中，矩阵乘法使用 @ 符号，转置使用 .T
+        B = np.zeros((3, 3))
+        # 这是方程对 vx, vy, omega 求偏导
+        B[0, 0] = math.cos(theta)
+        B[0, 1] = -math.sin(theta)
+        B[1, 0] = math.sin(theta)
+        B[1, 1] = math.cos(theta)
+        B[2, 2] = 1.0
+        
+        return A, B
     
-    """
-    
-    K=np.linalg.inv(R)@ B.T @ P
-
-    return K
+    def solve_lqr(self, A, B):
+        #使用scipy，求解连续黎卡提方程，返回K矩阵
+        P = la.solve_continuous_are(A, B, self.Q, self.R)
+        K = np.linalg.inv(self.R) @ B.T @ P
+        return K
 
--- lqr_cart_control/lqr_node.py
+++ lqr_cart_control/lqr_node.py
@@ -1,60 +1,81 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32MultiArray, Float32
+from std_msgs.msg import Float32MultiArray
+from geometry_msgs.msg import Twist
 import numpy as np
-# 导入写的数学引擎
-from .lqr_math import compute_lqr_gain
+from lqr_cart_control.lqr_math import LqrMathEngine
+import math
 
-class LQRControllerNode(Node):
+
+class LqrMecanumController(Node):
     def __init__(self):
-        super().__init__("lqr_node")
-        #获取当前时间
-        self.state_time=self.get_clock().now()
-        #动力学参数（A,B） 要和物理法则一致
-        self.A=np.array([[0.0,1.0],[0.0,-0.1]])
-        self.B=np.array([[0.0],[1.0]])
-
-        #LQR权重参数（Q,R） Q对误差的惩罚 R对能量消耗的惩罚
-        self.Q=np.array([[10.0,0.0],[0.0,1.0]])
-        self.R=np.array([[1.0]])
-
-        #目标设定 最终希望 p=5.0 v=0.0
-        self.target=np.array([[5.0],[0.0]])
-
-        #物理环境不变，K 矩阵只需要算一次
-        self.K = compute_lqr_gain(self.A, self.B, self.Q, self.R)
-        self.get_logger().info(f'成功计算最优增益矩阵 K: {self.K}')
-
-        #创建订阅者获取v p
-        self.sub_state = self.create_subscription(
-            Float32MultiArray, '/cart/state', self.state_callback, 10)    
-        #创建发布者发布控制力 
-        self.pub_u = self.create_publisher(Float32, '/lqr/control_input', 10)
+        super().__init__("lqr_mecanum_controller")
+        #创建订阅发布者
+        self.sub_state = self.create_subscription(Float32MultiArray, '/cart/state_2d', self.state_callback, 10)
+        self.pub_cmd = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.pub_target = self.create_publisher(Float32MultiArray, '/cart/target_2d', 10)
+        self.math_engine = LqrMathEngine()
+        self.current_state = np.zeros((3, 1)) 
+        self.last_u = np.zeros((3, 1))        
+        self.start_time = self.get_clock().now()
+        #创建定时器进行发布
+        self.timer = self.create_timer(0.01, self.control_loop)
+        self.get_logger().info('Initialization successful')
 
     def state_callback(self,msg):
-        #计算时间差
-        now=self.get_clock().now()
-        t=(now-self.state_time).nanoseconds /1e9
+        #更新数据
+        self.current_state[0, 0] = msg.data[0] 
+        self.current_state[1, 0] = msg.data[1] 
+        self.current_state[2, 0] = msg.data[2]
 
-        #这里让目标点呈时间正弦变化，追踪动态目标点
-        target_p=5.0*np.sin(0.5*t)
-        target_v = 2.5 * np.cos(0.5 * t)
-        self.target = np.array([[target_p], [target_v]])
+    def control_loop(self):
+        t = (self.get_clock().now() - self.start_time).nanoseconds / 1e9
+        #生成8字形目标
+        target_x = 3.0 * math.sin(0.5 * t)
+        target_y = 3.0 * math.sin(1.0 * t)
+        target_theta = 0.0  
+        v_target_global_x = 1.5 * math.cos(0.5 * t)
+        v_target_global_y = 3.0 * math.cos(1.0 * t)
+        #发布目标位置
+        target_msg = Float32MultiArray()
+        target_msg.data = [target_x, target_y, target_theta]
+        self.pub_target.publish(target_msg)   
+        # --- 步骤 2：前馈控制 (逆投影) ---
+        theta = float(self.current_state[2, 0])
+        u_ff_x = v_target_global_x * math.cos(theta) + v_target_global_y * math.sin(theta)
+        u_ff_y = -v_target_global_x * math.sin(theta) + v_target_global_y * math.cos(theta)
+        u_ff_omega = 0.0 
+        u_ff = np.array([[u_ff_x], [u_ff_y], [u_ff_omega]]) 
+        
+        #LQR 反馈控制
+        e_x = self.current_state[0, 0] - target_x
+        e_y = self.current_state[1, 0] - target_y
+        e_theta = self.current_state[2, 0] - target_theta
+        e_theta = math.atan2(math.sin(e_theta), math.cos(e_theta))
+        error = np.array([[e_x], [e_y], [e_theta]])
+        
+        last_vx = float(self.last_u[0, 0])
+        last_vy = float(self.last_u[1, 0])
+        
+        A, B = self.math_engine.get_linearized_dynamics(theta, last_vx, last_vy)
+        K = self.math_engine.solve_lqr(A, B)
+        u_fb = -K @ error
+        
+        #指令下发
+        u_total = u_ff + u_fb
+        self.last_u = u_total 
+        
+        cmd_msg = Twist()
+        cmd_msg.linear.x = float(u_total[0, 0])
+        cmd_msg.linear.y = float(u_total[1, 0])
+        cmd_msg.angular.z = float(u_total[2, 0])
+        self.pub_cmd.publish(cmd_msg)
 
-        #处理v p数据
-        current = np.array([[msg.data[0]], [msg.data[1]]])
-        #计算误差 (Error = Current - Target)
-        error = current - self.target
-        #核心控制律计算：u = -K * error
-        u_matrix = -self.K @ error       
-        #发布控制力 (取矩阵里的第一个标量值)
-        control_msg = Float32()
-        control_msg.data = float(u_matrix[0, 0])
-        self.pub_u.publish(control_msg)
+
 
 def main(args=None):
     rclpy.init(args=args)
-    node = LQRControllerNode()
+    node = LqrMecanumController()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
